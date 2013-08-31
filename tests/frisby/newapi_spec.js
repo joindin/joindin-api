81c51c03151b53c245a45b88365279505f6ee737
Lorna Mitchell <lorna@lornajane.net>
2013-08-31 16:08:33 +0100

    updating the tests to match what I just renamed the URL to

--- tests/frisby/newapi_spec.js
+++ tests/frisby/newapi_spec.js
@@ -185,7 +185,7 @@ function checkEvent(ev) {
   expect(ev.comments_uri).toBeDefined();
   expect(ev.talks_uri).toBeDefined();
   expect(ev.website_uri).toBeDefined();
-  expect(ev.attendance_uri).toBeDefined();
+  expect(ev.attending_uri).toBeDefined();
   expect(typeof ev.name).toBe('string');
   checkDate(ev.start_date);
   checkDate(ev.end_date);
