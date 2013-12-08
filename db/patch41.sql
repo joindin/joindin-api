ec89450c46288530c122c7c52a9903ab09967cee
Lorna Mitchell <lorna@lornajane.net>
2013-12-08 13:03:19 +0000

    adding a constraint to avoid users ending up attending events more than once

---
+++ db/patch41.sql
@@ -0,0 +1,7 @@
+-- add a unique constraint so that each user can only be attending
+-- each event once
+
+alter table user_attend add unique index idx_unique_user_event (uid, eid);
+
+INSERT INTO patch_history SET patch_number = 41;
+
