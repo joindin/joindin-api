faa7065a9de4a329dfb0d2539ddf1f330928f65d
Lorna Mitchell <lorna@lornajane.net>
2013-12-17 20:27:03 +0000

    use existing source fields

--- db/patch41.sql
+++
@@ -1,7 +0,0 @@
--- Add a column for API key source
-
-ALTER TABLE `event_comments` ADD consumer_id INT;
-ALTER TABLE `talk_comments` ADD consumer_id INT;
-
--- Bump patch version
-INSERT INTO patch_history SET patch_number = 41;
