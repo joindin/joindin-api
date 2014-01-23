5b5860c8d981f4bea1aec17e6e4ced3bb9b0344e
Rich Sage <rich.sage@gmail.com>
2014-01-23 10:37:53 +0000

    Patch script changed to "star" from "attend".

--- db/patch43.sql
+++ db/patch43.sql
@@ -1,19 +1,19 @@
 --
--- Table structure for table `user_talk_attend`
+-- Table structure for table `user_talk_star`
 --
 
-DROP TABLE IF EXISTS `user_talk_attend`;
-CREATE TABLE `user_talk_attend` (
+DROP TABLE IF EXISTS `user_talk_star`;
+CREATE TABLE `user_talk_star` (
   `uid` int(11) NOT NULL,
   `tid` int(11) NOT NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-ALTER TABLE user_talk_attend ADD INDEX idx_talk (tid);
+ALTER TABLE user_talk_star ADD INDEX idx_talk (tid);
 
--- Add a unique constraint so that each user can only be attending each talk once
+-- Add a unique constraint so that each user can only star each talk once
 
-ALTER TABLE user_talk_attend ADD UNIQUE INDEX idx_unique_user_talk (uid, tid);
+ALTER TABLE user_talk_star ADD UNIQUE INDEX idx_unique_user_talk (uid, tid);
 
 INSERT INTO patch_history SET patch_number = 43;
