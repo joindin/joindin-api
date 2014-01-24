40a4aed96aef9179643b19d22b8e50217036e806
Lorna Mitchell <lorna@lornajane.net>
2014-01-24 21:54:30 +0000

    don't use json_numeric_check, but check each field and allow some exceptions that look like numbers but are not

--- src/views/JsonView.php
+++ src/views/JsonView.php
@@ -12,8 +12,32 @@ class JsonView extends ApiView {
      */
     public function buildOutput ($content) {
         $content = $this->addCount($content);
-        $retval =  json_encode($content, JSON_NUMERIC_CHECK);
+        // need to work out which fields should have been numbers
+        // Don't use JSON_NUMERIC_CHECK because it eats talk stubs
+        $output = $this->numeric_check($content);
+        $retval = json_encode($output);
         return $retval;
+    }
+
+    protected function numeric_check($data)
+    {
+        $output = array();
+        foreach($data as $key => $value) {
+            
+            // recurse as needed
+            if(is_array($value)) {
+                $output[$key] = $this->numeric_check($value);
+            } else {
+
+                // stubs are hex, but can look like scientific notation
+                if(is_numeric($value) && $key != "stub") {
+                    $output[$key] = (float)$value;
+                } else {
+                    $output[$key] = $value;
+                }
+            }
+        }
+        return $output;
     }
 
 }
