7dded922d7fbbcf89dea73cbd22be89a00a11989
Lorna Mitchell <lorna@lornajane.net>
2014-05-04 20:46:54 +0100

    tidy up database patch and put patch history entry as the last thing so we know it worked if we get that

--- db/patch46.sql
+++ db/patch46.sql
@@ -1,9 +1,13 @@
 -- Add cache count columns for talks and tracks
 
-INSERT INTO patch_history SET patch_number = 46;
-
-ALTER TABLE events ADD comment_count INT(10) DEFAULT 0, ADD talk_count INT(10) DEFAULT 0, ADD track_count INT(10) DEFAULT 0;
+ALTER TABLE events 
+  ADD comment_count int unsigned not null DEFAULT 0, 
+  ADD talk_count int unsigned not null DEFAULT 0, 
+  ADD track_count int unsigned not null DEFAULT 0;
 
 UPDATE events e SET comment_count = (SELECT COUNT(*) FROM event_comments ec WHERE ec.event_id = e.ID);
 UPDATE events e SET talk_count = (SELECT COUNT(*) FROM talks t WHERE t.event_id = e.ID);
 UPDATE events e SET track_count = (SELECT COUNT(*) FROM event_track et WHERE et.event_id = e.ID);
+
+INSERT INTO patch_history SET patch_number = 46;
+
