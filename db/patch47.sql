93569452ceb07bedf2e0f17e9ba58ca2994df720
Rich Sage <rich.sage@gmail.com>
2014-06-19 14:55:15 +0100

    Add patch number bump.

--- db/patch47.sql
+++ db/patch47.sql
@@ -3,3 +3,5 @@
 
 ALTER TABLE `event_comments` CHANGE `source` `source` VARCHAR(255);
 ALTER TABLE `talk_comments` CHANGE `source` `source` VARCHAR(255);
+
+INSERT INTO patch_history SET patch_number = 47;
