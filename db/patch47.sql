96437885e4c5b00760bb8b2c547354c4a9268172
Rich Sage <rich.sage@gmail.com>
2014-06-19 14:39:33 +0100

    Fix "source" column length for comments. It now matches the length of the data potentially being inserted into it.

---
+++ db/patch47.sql
@@ -0,0 +1,5 @@
+# Update source field length on comment tables
+# to match the length of the API key "application" column
+
+ALTER TABLE `event_comments` CHANGE `source` `source` VARCHAR(255);
+ALTER TABLE `talk_comments` CHANGE `source` `source` VARCHAR(255);
