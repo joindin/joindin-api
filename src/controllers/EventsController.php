4aa5a165dafd787a9353b46a72e14cb1c24cbb86
Scott Wilcox <scott@dor.ky>
2014-09-25 20:50:11 +0100

    Change if() login to handle array index not being present

--- src/controllers/EventsController.php
+++ src/controllers/EventsController.php
@@ -117,7 +117,7 @@ class EventsController extends ApiController {
     public function postAction($request, $db) {
 
         if(!isset($request->user_id)) {
-            if ((isset($request->url_elements[4])) && ($request->url_elements[4] != "messages")) {
+            if ( (!isset($request->url_elements[4])) || ((isset($request->url_elements[4])) && ($request->url_elements[4] != "messages")) ) {
                 throw new Exception("You must be logged in to create data", 400);
             }
         }
