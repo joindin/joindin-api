ff72aa76389bd51c5488f5949b623003c4db3437
Rob Allen <rob@akrabat.com>
2014-12-06 20:48:41 +0000

    Extract the url in the registration email into a new config variable called website_url.

--- src/services/UserRegistrationEmailService.php
+++ src/services/UserRegistrationEmailService.php
@@ -3,13 +3,15 @@
 class UserRegistrationEmailService extends EmailBaseService
 {
     protected $token;
+    protected $website_url;
 
     public function __construct($config, $recipients, $token)
     {
         // set up the common stuff first
         parent::__construct($config, $recipients);
 
-        $this->token = $token;
+        $this->token       = $token;
+        $this->website_url = $config['website_url'];
     }
 
     public function sendEmail()
@@ -17,7 +19,8 @@ class UserRegistrationEmailService extends EmailBaseService
         $this->setSubject('Welcome to joind.in');
 
         $replacements = array(
-            "token"       => $this->token,
+            "token"   => $this->token,
+            "website_url" => $this->website_url,
         );
 
         $messageBody = $this->parseEmail("userRegistration.md", $replacements);
--- src/views/emails/userRegistration.md
+++ src/views/emails/userRegistration.md
@@ -1,5 +1,5 @@
 Thanks for registering on joind.in!  In order to verify your email address, please click the link below:
 
-[http://m.joind.in/user/verification?token=[token]](http://m.joind.in/user/verification?token=[token])
+[[website_url]/user/verification?token=[token]]([website_url]/user/verification?token=[token])
 
 
