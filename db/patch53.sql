b98a9ed011a004d3ab92b1024f64fee122156f27
Kevin <kevin@magicmonkey.org>
2015-02-22 17:15:14 +0000

    Some indexes, from observation of a couple of minutes of the slow query log

---
+++ db/patch53.sql
@@ -0,0 +1,6 @@
+alter table tags_events add index idx_eventid (event_id);
+alter table event_comments add index idx_userid (user_id);
+alter table talk_speaker add index idx_speakerid (speaker_id);
+alter table pending_talk_claims add index idx_talkid (talk_id);
+INSERT INTO patch_history SET patch_number = 53;
+
