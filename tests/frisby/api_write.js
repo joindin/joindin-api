c2b5d9503eba10a2d348c5c9cef7892a2a7ef430
Lorna Mitchell <lorna@lornajane.net>
2015-03-08 18:42:13 +0000

    Test verified user (fails on platforms that can't autoverify)

--- tests/frisby/api_write.js
+++ tests/frisby/api_write.js
@@ -38,6 +38,30 @@ function testRegisterUser() {
 	.toss();
 }
 
+function testRegisterVerifiedUser() {
+	var randomSuffix = parseInt(Math.random() * 1000000).toString();
+  var username = "testUser" + randomSuffix;
+  var password = "pwpwpwpwpwpw";
+	frisby.create('Register user')
+		.post(baseURL + "/v2.1/users", {
+			"username"         : username,
+			"password"         : password,
+			"full_name"        : "A test user",
+			"email"            : "testuser"+randomSuffix+"@example.com",
+			"auto_verify_user" : "true"
+		}, {json:true})
+		.expectStatus(201)
+		.expectHeaderContains("Location", baseURL + "/v2.1/users")
+		.after(function(err, res, body) {
+      if(res.statusCode == 201) {
+        // Call the get user method on the place we're told to go
+        testUserByUrl(res.headers.location);
+        testUserLogin(username, password);
+      }
+		})
+	.toss();
+}
+
 function testUserByUrl(url) {
 	frisby.create('Get user')
 		.get(url)
@@ -63,7 +87,29 @@ function testUnverifiedUserFailsLogin(username, password) {
 
 }
 
+function testUserLogin(username, password) {
+  frisby.create('Log in')
+    .post(baseURL + "/v2.1/token", {
+      "grant_type": "password",
+      "username": username,
+      "password": password,
+      "client_id": "0000",
+      "client_secret": "1111"
+    }, {json:true})
+    .expectStatus(200) // fails if user didn't get autoverified
+    .after(function(error, res, data) {
+      if(res.statusCode == 200) {
+        expect(data.access_token).toBeDefined();
+        expect(typeof data.access_token).toBe('string');
+        expect(data.user_uri).toBeDefined();
+        expect(typeof data.user_uri).toBe('string');
+      }
+    })
+    .toss();
+}
+
 module.exports = {
 	init      : init,
-	testRegisterUser : testRegisterUser
+	testRegisterUser : testRegisterUser,
+	testRegisterVerifiedUser : testRegisterVerifiedUser
 }
