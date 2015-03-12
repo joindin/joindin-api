d4f13067ff450fb2546cf53b29357597a7ab91a3
Lorna Mitchell <lorna@lornajane.net>
2015-03-12 20:22:28 +0000

    let a user request a reminder of their username by email

--- src/controllers/EmailsController.php
+++ src/controllers/EmailsController.php
@@ -32,4 +32,28 @@ class EmailsController extends ApiController {
             throw new Exception("Can't find that email address", 400);
         }
     }
+
+    public function username_reminder($request, $db) {
+        $user_mapper= new UserMapper($db, $request);
+        $email = filter_var($request->getParameter("email"), FILTER_VALIDATE_EMAIL);
+        if(empty($email)) {
+            throw new Exception("The email address must be supplied", 400);
+        } else {
+            // need the user's ID rather than the representation
+            $user_id = $user_mapper->getUserIdFromEmail($email);
+            if($user_id) {
+                $list = $user_mapper->getUserById($user_id);
+                $user = $list['users'][0];
+
+                $recipients = array($email);
+                $emailService = new UserUsernameReminderEmailService($this->config, $recipients, $user);
+                $emailService->sendEmail();
+
+                header("Content-Length: 0", NULL, 202);
+                exit;
+            }
+            throw new Exception("Can't find that email address", 400);
+        }
+    }
+
 }
