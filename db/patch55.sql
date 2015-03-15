98b78c0012f56bd16d8d2c98d1f07dca8ce4b1d8
Lorna Mitchell <lorna@lornajane.net>
2015-03-15 18:44:24 +0000

    Fix typo in db patch

--- db/patch55.sql
+++ db/patch55.sql
@@ -1,5 +1,5 @@
 -- Add the ratings column to the event_comments table
-ALTER TABLE event_comments ADD COLUMN `rating` int(11) default NULL, ;
+ALTER TABLE event_comments ADD COLUMN `rating` int(11) default NULL;
     
 -- Increase patch count
 INSERT INTO patch_history SET patch_number = 55;
