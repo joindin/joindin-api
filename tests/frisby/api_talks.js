3122363d7235afe5e205fe6771b30351372e8fab
Andreas Heigl <andreas@heigl.org>
2015-06-05 21:13:42 +0200

    Adds frisby tests for Talk-creation
    THis does not cover all eventualities and does not cover speaker-handling

--- tests/frisby/api_talks.js
+++ tests/frisby/api_talks.js
@@ -13,73 +13,99 @@ function init(_baseURL) {
 			headers: { 'Content-type': 'application/json' }
 		}
 	});
-}
 
-function testAddTalk() {
-  var randomSuffix = parseInt(Math.random() * 1000000).toString();
-  var username = "testUser" + randomSuffix;
-  var password = "pwpwpwpwpwpw";
-  var email = "testuser"+randomSuffix+"@example.com";
-	frisby.create('Register user')
-		.post(baseURL + "/v2.1/users", {
-			"username"  : username,
-			"password"  : password,
-			"full_name" : "A test user",
-			"email"     : email
-		}, {json:true})
-		.expectStatus(201)
-		.expectHeaderContains("Location", baseURL + "/v2.1/users")
-		.after(function(err, res, body) {
-      if(res.statusCode == 201) {
-        // Call the get user method on the place we're told to go
-        testUserByUrl(res.headers.location);
-        testUnverifiedUserFailsLogin(username, password);
-        testResendVerificationWorks(email);
-        testResendVerificationFails("doesntexist@lornajane.net");
-      }
-		})
-	.toss();
-}
-
-function testReadTalk(){}
-
-function testEditTalk() {
-	var randomSuffix = parseInt(Math.random() * 1000000).toString();
-  var username = "testUser" + randomSuffix;
-  var password = "pwpwpwpwpwpw";
-	frisby.create('Register user')
-		.post(baseURL + "/v2.1/users", {
-			"username"         : username,
-			"password"         : password,
-			"full_name"        : "A test user",
-			"email"            : "testuser"+randomSuffix+"@example.com",
-			"auto_verify_user" : "true"
-		}, {json:true})
-		.expectStatus(201)
-		.expectHeaderContains("Location", baseURL + "/v2.1/users")
-		.after(function(err, res, body) {
-      if(res.statusCode == 201) {
-        // Call the get user method on the place we're told to go
-        testUserByUrl(res.headers.location);
-        testUserLogin(username, password);
-      }
-		})
-	.toss();
-}
-
-function testRemoveTalk() {
-	frisby.create('Get user')
-		.get(url)
+	frisby.create('Login User')
+		.post(baseURL + "/v2.1/token", {
+			"username" : "imaadmin",
+			"password" : "password",
+			"grant_type" : "password",
+			"client_id" : "0000",
+			"client_secret" : "1111"
+		}, {json: true})
 		.expectStatus(200)
-		.expectJSONLength("users", 1)
-		.afterJSON(function (users) {
-			datatest.checkUserData(users.users[0]);
+		.after(function(err, res, body){
+			if (res.statusCode != 200) {
+				return;
+			}
+			testAddTalk(res.body.access_token);
 		})
-	.toss();
+		.toss()
+	;
+}
+
+function testAddTalk(token) {
+
+	var data = {
+		"talk_title"  : "My cool test-talk",
+		"talk_description"  : "Lorem Ipsum",
+		"start_date" : "21.12.2015 12:00",
+		"duration" : 45,
+		"langauge" : "English - UK",
+		"type" : "Talk",
+		"speakers" : [
+			"John Doe",
+			"Jane Bar"
+		]
+	};
+	frisby.create('Create Talk without login fails')
+		.post(baseURL + '/v2.1/events/44/talks', {},{json:true})
+		.expectStatus(401)
+	    .addHeader("Authorization", "oauth foo")
+		.after(function(err, res, body) {
+			if(res.statusCode != 401) {
+				console.log(body);
+				return;
+			}
+		})
+		.toss();
+	currentData = util._extend({},data);
+	currentData.talk_title = '';
+	frisby.create("CreateTalk without name")
+		.post(baseURL + '/v2.1/events/44/talks', currentData, {json:true})
+		.expectStatus(400)
+	    .addHeader("Authorization", "oauth " + token)
+		.toss();
+	currentData = util._extend({},data);
+	currentData.talk_description = '';
+	frisby.create("CreateTalk without name")
+		.post(baseURL + '/v2.1/events/44/talks', currentData, {json:true})
+		.expectStatus(400)
+	    .addHeader("Authorization", "oauth " + token)
+		.toss();
+	currentData = util._extend({},data);
+	frisby.create('Create Talk')
+		.post(baseURL + "/v2.1/events/44/talks", currentData, {json:true})
+	    .addHeader("Authorization", "oauth " + token)
+		.expectStatus(201)
+		.expectHeaderContains("Location", baseURL + "/v2.1/talks")
+		.after(function(err, res, body) {
+			if(res.statusCode != 201) {
+				console.log(body);
+				return;
+			}
+			testReadTalk(token, res.headers.location);
+		})
+		.toss();
+}
+
+function testReadTalk(token, talkURI){
+	frisby.create('Read Talk')
+		.get(talkURI, {}, {json: true})
+		.expectStatus(200)
+		.after(function(){
+			testDeleteTalk(token, talkURI);
+		})
+		.toss();
+}
+
+function testDeleteTalk(token, talkURI){
+	frisby.create("Delete Talk")
+		.delete(talkURI)
+		.addHeader("Authorization", "oauth " + token)
+		.expectStatus(204)
+		.toss();
 }
 
 module.exports = {
 	init      : init,
-	testRegisterUser : testAddTalk,
-	testRegisterVerifiedUser : testRegisterVerifiedUser
 }
