2f428354b95bdf61ada741f49b83947d27397c85
Lorna Jane Mitchell <lorna@lornajane.net>
2015-07-11 11:31:51 +0100

    refactoring the routing to match the new scheme

--- src/controllers/LanguagesController.php
+++ src/controllers/LanguagesController.php
@@ -3,16 +3,23 @@
 class LanguagesController extends ApiController
 {
     public function handle(Request $request, $db) {
-        // only GET is implemented so far
-        if ($request->getVerb() == 'GET') {
-            return $this->getAction($request, $db);
-        }
-        return false;
     }
 
-    public function getAction($request, $db) {
+    public function getLanguage($request, $db) {
         $language_id = $this->getItemId($request);
+        // verbosity - here for consistency as we don't have verbose language details to return at the moment
+        $verbose = $this->getVerbosity($request);
 
+        $mapper = new LanguageMapper($db, $request);
+        $list = $mapper->getLanguageById($language_id, $verbose);
+
+        if (count($list['languages']) == 0) {
+            throw new Exception('Language not found', 404);
+        }
+        return $list;
+    }
+
+    public function getAllLanguages($request, $db) {
         // verbosity - here for consistency as we don't have verbose language details to return at the moment
         $verbose = $this->getVerbosity($request);
 
@@ -21,16 +28,7 @@ class LanguagesController extends ApiController
         $resultsperpage = $this->getResultsPerPage($request);
 
         $mapper = new LanguageMapper($db, $request);
-
-        if ($language_id !== false) {
-            $list = $mapper->getLanguageById($language_id, $verbose);
-            if (count($list['languages']) == 0) {
-                throw new Exception('Language not found', 404);
-            }
-        } else {
-            $list = $mapper->getLanguageList($resultsperpage, $start, $verbose);
-        }
-
+        $list = $mapper->getLanguageList($resultsperpage, $start, $verbose);
         return $list;
     }
 }
