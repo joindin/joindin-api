17ffbfd8a18f7e9af34909e6d995617991612f4e
Andreas Heigl <andreas@heigl.org>
2015-08-25 15:51:18 +0200

    Move talk-creation to TalkController
    This commit moves talk creation from the Events-Controller to the Talks
    controller and adapts the routing appropriately.
    
    Additionally tests for minimum-talk creation have been added to make
    sure talk-creation works as expected.

--- src/controllers/TalksController.php
+++ src/controllers/TalksController.php
@@ -228,60 +228,61 @@ class TalksController extends ApiController
         if (count($list['events']) == 0) {
             throw new Exception('Event not found', 404);
         }
-            $event = $list['events'][0];
+        $event = $list['events'][0];
 
-            $talk['title'] = filter_var(
-                $request->getParameter('talk_title'),
-                FILTER_SANITIZE_STRING
-            );
-            if (empty($talk['title'])) {
-                throw new Exception("The talk title field is required", 400);
+        $talk['title'] = filter_var(
+            $request->getParameter('talk_title'),
+            FILTER_SANITIZE_STRING
+        );
+        if (empty($talk['title'])) {
+            throw new Exception("The talk title field is required", 400);
+        }
+
+        $talk['description'] = filter_var(
+            $request->getParameter('talk_description'),
+            FILTER_SANITIZE_STRING
+        );
+        if (empty($talk['description'])) {
+            throw new Exception("The talk description field is required", 400);
+        }
+
+        $talk_types = array("Talk", "Social event", "Keynote", "Workshop", "Event related");
+        if ($request->getParameter("talk_type") && in_array($request->getParameter("talk_type"), $talk_types)) {
+            $talk['talk_type'] = $request->getParameter("talk_type");
+        } else {
+            $talk['talk_type'] = "Talk";
+        }
+
+        $talk['language'] = filter_var($request->getParameter('language'), FILTER_SANITIZE_STRING);
+        if (empty($talk['language'])) {
+            // default to UK English
+            $talk['language'] = 'English - UK';
+        }
+
+        $start_date = $request->getParameter('start_date');
+        if (empty($start_date)) {
+            throw new Exception("Please give the date and time of the talk", 400);
+        }
+        $tz = new DateTimeZone($event['tz_continent'] . '/' . $event['tz_place']);
+        $start_date = new DateTime($request->getParameter("start_date"), $tz);
+        $talk['date'] = $start_date->format('U');
+
+        $speakers = $request->getParameter('speakers');
+        if (is_array($speakers)) {
+            foreach ($speakers as $speaker) {
+                $talk['speakers'][] = filter_var($speaker, FILTER_SANITIZE_STRING);
             }
+        }
 
-            $talk['description'] = filter_var(
-                $request->getParameter('talk_description'),
-                FILTER_SANITIZE_STRING
-            );
-            if (empty($talk['description'])) {
-                throw new Exception("The talk description field is required", 400);
-            }
+        $talk_mapper = new TalkMapper($db, $request);
+        $new_id = $talk_mapper->save($talk);
 
-            $talk_types = array("Talk", "Social event", "Keynote", "Workshop", "Event related");
-            if ($request->getParameter("talk_type") && in_array($request->getParameter("talk_type"), $talk_types)) {
-                $talk['talk_type'] = $request->getParameter("talk_type");
-            } else {
-                $talk['talk_type'] = "Talk";
-            }
+        // Update the cache count for the number of talks at this event
+        $event_mapper->cacheTalkCount($talk['event_id']);
 
-            $talk['language'] = filter_var($request->getParameter('language'), FILTER_SANITIZE_STRING);
-            if (empty($talk['language'])) {
-                // default to UK English
-                $talk['language'] = 'English - UK';
-            }
+        header("Location: " . $request->base . $request->path_info .'/' . $new_id, null, 201);
+        $new_talk = $talk_mapper->getTalkById($new_id);
 
-            $start_date = $request->getParameter('start_date');
-            if (empty($start_date)) {
-                throw new Exception("Please give the date and time of the talk", 400);
-            }
-            $tz = new DateTimeZone($event['tz_continent'] . '/' . $event['tz_place']);
-            $start_date = new DateTime($request->getParameter("start_date"), $tz);
-            $talk['date'] = $start_date->format('U');
-
-            $speakers = $request->getParameter('speakers');
-            if (is_array($speakers)) {
-                foreach ($speakers as $speaker) {
-                    $talk['speakers'][] = filter_var($speaker, FILTER_SANITIZE_STRING);
-                }
-            }
-
-            $talk_mapper = new TalkMapper($db, $request);
-            $new_id = $talk_mapper->save($talk);
-
-            // Update the cache count for the number of talks at this event
-            $event_mapper->cacheTalkCount($talk['event_id']);
-
-            header("Location: " . $request->base . $request->path_info .'/' . $new_id, null, 201);
-            $new_talk = $talk_mapper->getTalkById($new_id);
-            return $new_talk;
+        return $new_talk;
     }
 }
