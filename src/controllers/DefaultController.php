f019929c6cd180b19dcab3ada943c679d734c552
David Stockton <dave@davidstockton.com>
2015-09-06 14:03:16 -0600

    Codesniff - PSR-2 for DefaultController
    No functionality changes, just style

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -1,17 +1,19 @@
 <?php
 
-class DefaultController extends ApiController {
-	public function handle(Request $request, $db) {
+class DefaultController extends ApiController
+{
+    public function handle(Request $request, $db)
+    {
         $retval = array();
 
         // just add the available methods, with links
-        $retval['events'] = $request->base . '/' . $request->version . '/events';
-        $retval['hot-events'] = $request->base . '/' . $request->version . '/events?filter=hot';
+        $retval['events']          = $request->base . '/' . $request->version . '/events';
+        $retval['hot-events']      = $request->base . '/' . $request->version . '/events?filter=hot';
         $retval['upcoming-events'] = $request->base . '/' . $request->version . '/events?filter=upcoming';
-        $retval['past-events'] = $request->base . '/' . $request->version . '/events?filter=past';
-        $retval['open-cfps'] = $request->base . '/' . $request->version . '/events?filter=cfp';
-        $retval['docs'] = 'http://joindin.github.io/joindin-api/';
+        $retval['past-events']     = $request->base . '/' . $request->version . '/events?filter=past';
+        $retval['open-cfps']       = $request->base . '/' . $request->version . '/events?filter=cfp';
+        $retval['docs']            = 'http://joindin.github.io/joindin-api/';
 
         return $retval;
-	}
+    }
 }
