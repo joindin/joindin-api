c62f6c8111cc78bd4c03cc2fa09a0f7180e51578
David Stockton <dave@davidstockton.com>
2015-09-06 16:58:45 -0600

    Codesniff - PSR-2 update for TokenController
    No functionality changes

--- src/controllers/TokenController.php
+++ src/controllers/TokenController.php
@@ -11,38 +11,41 @@ class TokenController extends ApiController
         // password for an access token. This is used by web2.
 
         $grantType = $request->getParameter('grant_type');
-        $username = $request->getParameter('username');
-        $password = $request->getParameter('password');
+        $username  = $request->getParameter('username');
+        $password  = $request->getParameter('password');
 
         // all fields are required or this makes no sense
-        if(empty($grantType)) {
+        if (empty($grantType)) {
             throw new Exception('The field "grant_type" is required', 400);
         }
 
-        if(empty($username) || empty($password)) {
+        if (empty($username) || empty($password)) {
             throw new Exception('The fields "username" and "password" are both required', 400);
         }
 
         if ($grantType == 'password') {
             // authenticate the user for web2
 
-            $clientId = $request->getParameter('client_id');
+            $clientId     = $request->getParameter('client_id');
             $clientSecret = $request->getParameter('client_secret');
-            if (!$this->oauthModel->isClientPermittedPasswordGrant($clientId, $clientSecret)) {
+            if (! $this->oauthModel->isClientPermittedPasswordGrant($clientId, $clientSecret)) {
                 throw new Exception("This client cannot authenticate using the password grant type", 403);
             }
 
             // expire any old tokens
-            if(isset($this->config['oauth']['expirable_client_ids'])) {
+            if (isset($this->config['oauth']['expirable_client_ids'])) {
                 $this->oauthModel->expireOldTokens($this->config['oauth']['expirable_client_ids']);
             }
 
             // generate a temporary access token and then redirect back to the callback
             $username = $request->getParameter('username');
             $password = $request->getParameter('password');
-            $result  = $this->oauthModel->createAccessTokenFromPassword(
-                $clientId, $username, $password);
-    
+            $result   = $this->oauthModel->createAccessTokenFromPassword(
+                $clientId,
+                $username,
+                $password
+            );
+
             if ($result) {
                 return array('access_token' => $result['access_token'], 'user_uri' => $result['user_uri']);
             }
@@ -52,5 +55,4 @@ class TokenController extends ApiController
 
         throw new Exception("Grant type not recognised", 400);
     }
-
 }
