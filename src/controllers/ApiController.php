5e0a55972af41d5a92b4a2f9922f43d4ab38e364
David Stockton <dave@davidstockton.com>
2015-09-06 13:43:00 -0600

    Codesniff PSR-2 updates
    No functionality changes

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -1,48 +1,60 @@
 <?php
 
-abstract class ApiController {
+abstract class ApiController
+{
     protected $config;
 
-    public function __construct($config = null) {
+    public function __construct($config = null)
+    {
         $this->config = $config;
     }
 
-    public function getItemId($request) {
+    public function getItemId($request)
+    {
         // item ID
-		if(!empty($request->url_elements[3]) && is_numeric($request->url_elements[3])) {
-            $item_id = (int)$request->url_elements[3];
+        if (! empty($request->url_elements[3])
+            && is_numeric($request->url_elements[3])
+        ) {
+            $item_id = (int) $request->url_elements[3];
+
             return $item_id;
-		}
+        }
+
         return false;
     }
 
-    public function getVerbosity($request) {
+    public function getVerbosity($request)
+    {
         // verbosity
-        if(isset($request->parameters['verbose'])
-                && $request->parameters['verbose'] == 'yes') {
+        if (isset($request->parameters['verbose'])
+            && $request->parameters['verbose'] == 'yes'
+        ) {
             $verbose = true;
         } else {
             $verbose = false;
         }
+
         return $verbose;
     }
 
-    public function getStart($request) {
-        return (int)$request->paginationParameters['start'];
-         
-    }
-    
-    public function getResultsPerPage($request) {
-        return (int)$request->paginationParameters['resultsperpage'];
+    public function getStart($request)
+    {
+        return (int) $request->paginationParameters['start'];
+
     }
 
-    public function getSort($request) {
+    public function getResultsPerPage($request)
+    {
+        return (int) $request->paginationParameters['resultsperpage'];
+    }
+
+    public function getSort($request)
+    {
         // unfiltered, you probably want to switch case this
-        if(isset($request->parameters['sort'])) {
+        if (isset($request->parameters['sort'])) {
             return $request->parameters['sort'];
         } else {
             return false;
         }
     }
-    
 }
