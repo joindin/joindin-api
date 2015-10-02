91537a45e69c3fce62b5184599dfbac8d7ebb45d
Lorna Jane Mitchell <lorna@lornajane.net>
2015-10-02 21:51:18 +0100

    More verbose name to indicate which user

--- src/models/EventMapper.php
+++ src/models/EventMapper.php
@@ -989,10 +989,10 @@ class EventMapper extends ApiMapper
      * Approve a pending event
      *
      * @param  integer $event_id
-     * @param  integer $user_id
+     * @param  integer $reviewing_user_id The user that approved this event
      * @return boolean
      */
-    public function approve($event_id, $user_id)
+    public function approve($event_id, $reviewing_user_id)
     {
         $sql = "select ID from events where pending = 1 and active = 0 and ID = :event_id";
         $stmt = $this->_db->prepare($sql);
@@ -1003,21 +1003,19 @@ class EventMapper extends ApiMapper
             return false;
         }
 
-        $sql = "update events set pending = 0, active = 1, reviewer_id = :user_id where ID = :event_id";
+        $sql = "update events set pending = 0, active = 1, reviewer_id = :reviewing_user_id where ID = :event_id";
         $stmt = $this->_db->prepare($sql);
-        return $stmt->execute(["event_id" => $event_id, "user_id" => $user_id]);
+        return $stmt->execute(["event_id" => $event_id, "reviewing_user_id" => $reviewing_user_id]);
     }
 
     /**
      * Reject a pending event
      *
-     * Currently, this is done by deleting it
-     *
      * @param  integer $event_id
-     * @param  integer $user_id
+     * @param  integer $reviewing_user_id The user who rejected the event
      * @return boolean
      */
-    public function reject($event_id, $user_id)
+    public function reject($event_id, $reviewing_user_id)
     {
         $sql = "select ID from events where pending = 1 and active = 0 and ID = :event_id";
         $stmt = $this->_db->prepare($sql);
@@ -1027,8 +1025,8 @@ class EventMapper extends ApiMapper
             return false;
         }
 
-        $sql = "update events set pending = 0, active = 0, reviewer_id = :user_id where ID = :event_id";
+        $sql = "update events set pending = 0, active = 0, reviewer_id = :reviewing_user_id where ID = :event_id";
         $stmt = $this->_db->prepare($sql);
-        return $stmt->execute(["event_id" => $event_id, "user_id" => $user_id]);
+        return $stmt->execute(["event_id" => $event_id, "reviewing_user_id" => $reviewing_user_id]);
     }
 }
