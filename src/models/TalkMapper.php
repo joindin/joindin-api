d64b5ee1a856efa052b1fa357c9ea5b93fcdcea1
Lorna Jane Mitchell <lorna@lornajane.net>
2015-11-23 15:30:20 +0000

    Account for inactive comments when calculating the count

--- src/models/TalkMapper.php
+++ src/models/TalkMapper.php
@@ -227,7 +227,8 @@ class TalkMapper extends ApiMapper
     public function getBasicSQL()
     {
         $sql = 'select t.*, l.lang_name, e.event_tz_place, e.event_tz_cont, '
-               . '(select COUNT(ID) from talk_comments tc where tc.talk_id = t.ID and tc.private = 0)
+               . '(select COUNT(ID) from talk_comments tc where tc.talk_id = t.ID
+                and tc.private = 0 and tc.active = 1)
                 as comment_count, '
                . '(select get_talk_rating(t.ID)) as avg_rating, '
                . '(select count(*) from user_talk_star where user_talk_star.tid = t.ID)
