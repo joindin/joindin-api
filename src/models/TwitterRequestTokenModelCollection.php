4ddfc662bf179799892e455beae137f2211192cb
Rob Allen <rob@akrabat.com>
2015-12-06 09:49:29 +0000

    Extract addPaginationLinks() to AbstractModelCollection
    This method is common to all ModelCollections so abstract it rather than
    repeat it everywhere.

--- src/models/TwitterRequestTokenModelCollection.php
+++ src/models/TwitterRequestTokenModelCollection.php
@@ -4,15 +4,12 @@
  * Container for multiple TwitterRequestTokenModel objects, also handles
  * collection metadata such as pagination
  */
-class TwitterRequestTokenModelCollection
+class TwitterRequestTokenModelCollection extends AbstractModelCollection
 {
-    protected $list = array();
-    protected $total;
-
     /**
      * Take arrays of data and create a collection of models; store metadata
      */
-    public function __construct($data, $total = 0)
+    public function __construct(array $data, $total = 0)
     {
         $this->total = $total;
 
@@ -42,38 +39,5 @@ class TwitterRequestTokenModelCollection
         $retval['meta'] = $this->addPaginationLinks($request);
 
         return $retval;
-    }
-
-    /**
-     * Direct port of the function in ApiMapper
-     *
-     * Adds count, total, and this_page links.  Also adds next_page and prev_page
-     * as appropriate
-     */
-    protected function addPaginationLinks($request)
-    {
-        $count         = count($this->list);
-        $meta['count'] = $count;
-
-        $meta['total']     = $this->total;
-        $meta['this_page'] = $request->base . $request->path_info . '?' .
-                             http_build_query($request->paginationParameters);
-        $next_params       = $prev_params = $counter_params = $request->paginationParameters;
-        $firstOnNextPage   = $counter_params['start'] + $counter_params['resultsperpage'];
-        $firstOnThisPage   = $counter_params['start'];
-
-        if ($firstOnNextPage < $this->total) {
-            $next_params['start'] = $next_params['start'] + $next_params['resultsperpage'];
-            $meta['next_page']    = $request->base . $request->path_info . '?' . http_build_query($next_params);
-        }
-        if (0 < $firstOnThisPage) {
-            $prev_params['start'] = $prev_params['start'] - $prev_params['resultsperpage'];
-            if ($prev_params['start'] < 0) {
-                $prev_params['start'] = 0;
-            }
-            $meta['prev_page'] = $request->base . $request->path_info . '?' . http_build_query($prev_params);
-        }
-
-        return $meta;
     }
 }
