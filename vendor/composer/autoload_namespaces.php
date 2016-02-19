eda9e84c9545988dbffe855bc8bfdd927c390536
Kayla Daniels <kayladnls@gmail.com>
2016-02-19 15:42:40 +0000

    require phpunit

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Michelf' => array($vendorDir . '/michelf/php-markdown'),
 );
