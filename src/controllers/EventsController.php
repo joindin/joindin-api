9bbd0b6823b811efc18aa2b1e4d66e8d19fd179f
Jonny Stirling <jonny@ryudigital.com>
2016-05-07 17:42:29 +0100

    Added condition for start date > end date to error

--- src/controllers/EventsController.php
+++ src/controllers/EventsController.php
@@ -203,6 +203,8 @@ class EventsController extends ApiController
             $end_date   = strtotime($request->getParameter("end_date"));
             if (! $start_date || ! $end_date) {
                 $errors[] = "Both 'start_date' and 'end_date' must be supplied in a recognised format";
+            } else if ( $start_date > $end_date ) {
+                $errors[] = "The event start date must be before its end date";
             } else {
                 // if the dates are okay, sort out timezones
 
