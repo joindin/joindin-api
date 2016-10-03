1e68019199bb8a3bc0e2a7d524d62716e1ec98f1
Liam Wiltshire <liam@w.iltshi.re>
2016-10-04 00:44:05 +0100

    Adding claimTalk and assignTalk methods

--- src/models/PendingTalkClaimMapper.php
+++ src/models/PendingTalkClaimMapper.php
@@ -45,16 +45,26 @@ class PendingTalkClaimMapper extends ApiMapper
         return $fields;
     }
 
-    public function claimTalkAsSpeaker($talk_id){
+    public function claimTalkAsSpeaker($talk_id,$speaker_id,$claim_id)
+    {
         $sql  = 'insert into pending_talk_claims 
                     (talk_id,submitted_by,speaker_id,date_added,claim_id,user_approved_at) 
                   values
-                    (:talk_id,:submitted_by,:speaker_id,NOW(),:claim_id,NOW())
+                    (:talk_id,:submitted_by,:speaker_id,UNIX_TIMESTAMP(),:claim_id,NOW())
                  ';
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute(array('uid' => $user_id, 'tid' => $talk_id));
+        return $stmt->execute(array('talk_id' => $talk_id, 'submitted_by' => $speaker_id, 'speaker_id' => $speaker_id, 'claim_id' => $claim_id));
+    }
 
-        return true;
+    public function assignTalkAsHost($talk_id,$speaker_id,$claim_id,$user_id)
+    {
+        $sql  = 'insert into pending_talk_claims 
+                    (talk_id,submitted_by,speaker_id,date_added,claim_id,host_approved_at) 
+                  values
+                    (:talk_id,:submitted_by,:speaker_id,UNIX_TIMESTAMP(),:claim_id,NOW())
+                 ';
+        $stmt = $this->_db->prepare($sql);
+        return $stmt->execute(array('talk_id' => $talk_id, 'submitted_by' => $user_id, 'speaker_id' => $speaker_id, 'claim_id' => $claim_id));
     }
 
 
