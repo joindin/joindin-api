493be6ef071226f4dce1224c6c1e3bb130af915b
Liam Wiltshire <liam@w.iltshi.re>
2016-10-10 21:54:02 +0100

    Manual Merge

--- src/models/TalkMapper.php
+++ src/models/TalkMapper.php
@@ -796,4 +796,19 @@ class TalkMapper extends ApiMapper
         $stmt = $this->_db->prepare($sql);
         $stmt->execute($params);
     }
+    
+    public function assignTalkToSpeaker($talk_id, $claim_id, $speaker_id)
+    {
+        $sql = 'update talk_speaker 
+                SET speaker_id = :speaker_id
+                WHERE ID = :claim_id AND talk_id = :talk_id';
+        $stmt = $this->_db->prepare($sql);
+        return $stmt->execute(
+            [
+                'speaker_id'    => $speaker_id,
+                'talk_id'       => $talk_id,
+                'claim_id'      => $claim_id
+            ]
+        );
+    }
 }
