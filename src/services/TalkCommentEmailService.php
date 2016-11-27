06972fddfdf664622326a5fe28947d91653dd343
Scott Dutton <scott@exussum.co.uk>
2016-11-27 19:46:46 +0000

    JOINDIN-600
    Remove the if check

--- src/services/TalkCommentEmailService.php
+++ src/services/TalkCommentEmailService.php
@@ -23,10 +23,6 @@ class TalkCommentEmailService extends EmailBaseService
 
     public function sendEmail()
     {
-        if (empty($this->recipients)) {
-            return false;
-        }
-
         $this->setSubject("New feedback on " . $this->talk->talk_title);
 
         $byLine = '';
