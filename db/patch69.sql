b8f64c06019a727790dc1a009f138bca298431d1
Andreas Heigl <andreas@heigl.org>
2017-02-02 16:57:45 +0100

    Changes patch-number

---
+++ db/patch69.sql
@@ -0,0 +1,9 @@
+-- Add a new field `can_be_revoked` to the oauth_consumers table
+-- That field describes whether tokens from this consumer can be revoked by
+-- a user or not. Only tokens from consumers where that flag is set will be shown
+-- in the API endpoint /token/
+ALTER TABLE `oauth_consumers` ADD COLUMN can_be_revoked (tinyint(1) DEFAULT '1');
+
+UPDATE `oauth_consumers` SET can_be_revoked = 0 WHERE consumer_key = 'web2';
+
+INSERT INTO patch_history SET patch_number = 69;
