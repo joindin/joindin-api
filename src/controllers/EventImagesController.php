a7bb5dfa9750a81a00ef9817578c00eb08cae74d
Eric Poe <eric.poe@gmail.com>
2017-08-06 22:07:36 -0500

    Fix exception message

--- src/controllers/EventImagesController.php
+++ src/controllers/EventImagesController.php
@@ -27,7 +27,7 @@ class EventImagesController extends ApiController
         }
 
         if ($_FILES['image']['error'] != 0) {
-            throw new Exception("Image upload failed (Code: " . $FILES['image']['error'] . ")", 400);
+            throw new Exception("Image upload failed (Code: " . $_FILES['image']['error'] . ")", 400);
         }
 
         // check the file meets our expectations
