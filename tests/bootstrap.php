238867126539f19d6fbb9dde4116ee22ade168ef
Andreas Heigl <andreas@heigl.org>
2018-06-12 22:00:57 +0200

    Fix issues with composer-autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 // Load and register autoloader
 require_once __DIR__ . '/../src/inc/Autoloader.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+//require_once __DIR__ . '/../vendor/autoload.php';
 
 //We are unit testing
 define('UNIT_TEST', 1);
