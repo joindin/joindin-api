def2e4b2b475cf652918edf7044fb62a9de7f31c
Peter Ton <pton@ibuildings.nl>
2018-07-04 13:24:42 +0200

    Adjust database field type for user profile biography to be text

--- db/patch74.sql
+++ db/patch74.sql
@@ -1,5 +1,5 @@
 # adding the bio column to the user info
 
-ALTER TABLE `user` add `biography` VARCHAR(400);
+ALTER TABLE `user` add `biography` TEXT;
 
 INSERT INTO patch_history SET patch_number = 74;
