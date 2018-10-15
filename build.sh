f3b2ec0647626f6fa91d333f1be352c807032e3c
Scott Dutton <scott@exussum.co.uk>
2018-10-15 20:17:30 +0100

    Add syntax check

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+
+parallel-lint --exclude vendor .
+
 phpcs \
     --standard=tools/codesniffer/JoindInPSR2/ruleset.xml \
     --ignore=**/config.php,**/database.php,vendor,tools,tests/bootstrap.php \
