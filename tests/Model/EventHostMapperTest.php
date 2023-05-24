d5049844ee6eb376ccd64d901295c0bc569fea63
Ian Littman <iansltx@gmail.com>
2023-05-23 21:59:06 -0500

    Remove unneeded-due-to-types test, fix return type on test

--- tests/Model/EventHostMapperTest.php
+++ tests/Model/EventHostMapperTest.php
@@ -25,7 +25,7 @@ final class EventHostMapperTest extends TestCase
             ->with('INSERT INTO user_admin (uid, rid, rtype) VALUES (:host_id, :event_id, :type)')
             ->willReturn($stmt);
         $pdo->method('lastInsertId')
-            ->willReturn(14);
+            ->willReturn('14');
 
         $request = $this->getMockBuilder(Request::class)->disableOriginalConstructor()->getMock();
 
@@ -74,58 +74,6 @@ final class EventHostMapperTest extends TestCase
         $mapper = new EventHostMapper($pdo, $request);
 
         $this->assertFalse($mapper->getHostsByEventId(12, 10, 0));
-    }
-
-    public function testThatGettingHostByEventIdReturnsSomethingSensibleWhenStatementReturnsSingleValue()
-    {
-        $stmt1 = $this->getMockBuilder(PDOStatement::class)->disableOriginalConstructor()->getMock();
-        $stmt1->method('execute')
-            ->with([
-                ':event_id' => 12,
-            ])
-            ->willReturn(true);
-        $stmt1->method('fetchAll')
-            ->willReturn([
-                ['user_id' => 8, 'host_name' => 'Karl Napp'],
-            ]);
-        $stmt2 = $this->getMockBuilder(PDOStatement::class)->disableOriginalConstructor()->getMock();
-        $stmt2->method('execute')
-            ->with([
-                ':event_id' => 12,
-            ])
-            ->willReturn(true);
-        $stmt2->method('fetchAll')
-            ->willReturn(1);
-        $pdo = $this->getMockBuilder(PDO::class)->disableOriginalConstructor()->getMock();
-        $pdo->expects($this->at(0))
-            ->method('prepare')
-            ->with('select a.uid as user_id, u.full_name as host_name from user_admin a inner join user u on u.ID = a.uid where rid = :event_id and rtype="event" and (rcode!="pending" OR rcode is null) order by host_name  LIMIT 0,10')
-            ->willReturn($stmt1);
-        $pdo->expects($this->at(1))
-            ->method('prepare')
-            ->with('SELECT count(*) AS count FROM (select a.uid as user_id, u.full_name as host_name from user_admin a inner join user u on u.ID = a.uid where rid = :event_id and rtype="event" and (rcode!="pending" OR rcode is null) order by host_name  ) as counter')
-            ->willReturn($stmt2);
-        $request                       = $this->getMockBuilder(Request::class)->disableOriginalConstructor()->getMock();
-        $request->base                 = 'test';
-        $request->version              = '1';
-        $request->path_info            = '2';
-        $request->paginationParameters = ['resultsperpage' => 10, 'start' => 0];
-
-        $mapper = new EventHostMapper($pdo, $request);
-
-        $this->assertEquals([
-            'hosts' => [
-                [
-                    'host_name' => 'Karl Napp',
-                    'host_uri'  => 'test/1/users/8',
-                ]
-            ],
-            'meta'  => [
-                'count'     => 1,
-                'total'     => null,
-                'this_page' => 'test2?resultsperpage=10&start=0'
-            ]
-        ], $mapper->getHostsByEventId(12, 10, 0));
     }
 
     public function testThatRemovingAHostFromAnEventCallsTheExpectedMethods()
