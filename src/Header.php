765545ed3015a1cdcaceb869383be8f0e7d92f6c
Martin Herndl <martin@herndl.org>
2023-05-30 22:02:48 +0200

    Improve Header types and remove dead code

--- src/Header.php
+++ src/Header.php
@@ -89,7 +89,7 @@ class Header
         for ($i = 0, $total = count($values); $i < $total; $i++) {
             if (strpos($values[$i], $this->glue) !== false) {
                 // Explode on glue when the glue is not inside of a comma
-                foreach (preg_split('/' . preg_quote($this->glue, '/') . '(?=([^"]*"[^"]*")*[^"]*$)/', $values[$i]) as $v) {
+                foreach (preg_split('/' . preg_quote($this->glue, '/') . '(?=([^"]*"[^"]*")*[^"]*$)/', $values[$i]) ?: [] as $v) {
                     $values[] = trim($v);
                 }
                 unset($values[$i]);
@@ -130,6 +130,9 @@ class Header
         return count($this->toArray());
     }
 
+    /**
+     * @return ArrayIterator<int, array>
+     */
     public function getIterator(): ArrayIterator
     {
         return new ArrayIterator($this->toArray());
@@ -170,7 +173,7 @@ class Header
         foreach ($this->normalize()->toArray() as $val) {
             $part = [];
 
-            foreach (preg_split('/;(?=([^"]*"[^"]*")*[^"]*$)/', $val) as $kvp) {
+            foreach (preg_split('/;(?=([^"]*"[^"]*")*[^"]*$)/', $val) ?: [] as $kvp) {
                 if (!preg_match_all('/<[^>]+>|[^=]+/', $kvp, $matches)) {
                     continue;
                 }
